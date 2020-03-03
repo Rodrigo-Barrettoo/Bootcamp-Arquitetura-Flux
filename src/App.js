092e117d5c2118fa0f36ee56c80505d20ddf16bb
Rodrigo-Barrettoo <rodrigonarrettoo@outlook.com>
2020-03-03 09:15:14 -0300

    utilizando reducer para contar o tanto de item selecionado na sacola

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import GlobaStyle from './styles/global';
 import Header from './components/Header';
 import Routes from './routes';
 
+import store from './store';
+
 function App() {
   return (
-    <BrowserRouter>
-      <Header />
-      <GlobaStyle />
-      <Routes />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Header />
+        <GlobaStyle />
+        <Routes />
+      </BrowserRouter>
+    </Provider>
   );
 }
 
